72af75a7cdec1b6f2ca991ae803d51d5bf0c9f4d
oliver <oshmusic11@gmail.com>
2024-12-11 17:07:02 -0500

    Electron window launches

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Electron works!");
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadFile('src/index.html');
+}
+
+app.whenReady().then( () => {
+    createWindow();
+})
