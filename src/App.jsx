49bad78397a0d57134a8d6d26d9095879dc4c195
oliver <oshmusic11@gmail.com>
2024-12-18 16:45:45 -0500

    Added first draft of backend code to handle adding and removing players and incrementing the current turn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // Component imports
 import Turn from './components/Turn';
@@ -28,6 +28,9 @@ const testPlayers = [
 ];
 
 function App() {
+  // Index variable for iterating through Players state
+  const i = 0;
+
   // Players state
   const [players, setPlayers] = useState([]);
 
@@ -37,32 +40,56 @@ function App() {
   // Current Turn state
   const [currentTurn, setCurrentTurn] = useState(null);
 
+  // Ref to generate ids for players
+  const nextPlayerId = useRef(0);
 
-  // placeholder
-  const handleRemoveTurn = (id) => {
-    console.log(`Removed ${id}`);
+
+  const handleAddTurn = (name, initiative) => {
+    setPlayers(prevPlayers => [
+      ...prevPlayers,
+      {
+        name,
+        initiative,
+        id: nextPlayerId.current++
+      }
+    ]);
   }
 
- 
+  const handleRemoveTurn = (id) => {
+    console.log(`Removed ${id}`); //included for testing
+    setPlayers(prevPlayers => prevPlayers.filter(p => p.id !== id));
+  }
+
+  const handleNextTurn = () => {
+    i++;
+    if (i >= players.length) {
+      //if we've reached the end of the list of turns, reset the index variable and increase the round counter
+      i = 0;
+      setRound(round + 1);
+    }
+    currentTurn = players[i].id;
+  }
+
+
   return (
     <div>
       <p>Test!</p>
       <div>
         { /* Turns go here */
-        testPlayers.map(player =>
-          <Turn 
-            name={player.name}
-            initiative={player.initiative}
-            id={player.id}
-            key={player.id.toString()}
-            removeTurn={handleRemoveTurn}
-            isCurrentTurn={player.initiative === currentTurn}
-          />
-        )}
+          testPlayers.map(player =>
+            <Turn
+              name={player.name}
+              initiative={player.initiative}
+              id={player.id}
+              key={player.id.toString()}
+              removeTurn={handleRemoveTurn}
+              isCurrentTurn={player.id === currentTurn}
+            />
+          )}
         { /* AddTurnForm */}
       </div>
       <div>
-        { /* Sidebar */ }
+        { /* Sidebar */}
       </div>
     </div>
   )
