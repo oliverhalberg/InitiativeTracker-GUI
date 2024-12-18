b3d30e598ea810bc24c838a5bd7799aae6161be5
oliver <oshmusic11@gmail.com>
2024-12-18 16:14:27 -0500

    Added some scaffolding/planning - added players, round, currentTurn states

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Players state
+  const [players, setPlayers] = useState([]);
+
+  // Round state
+  const [round, setRound] = useState(1);
+
+  // Current Turn state
+  const [currentTurn, setCurrentTurn] = useState(null);
+
+  
+
 
   return (
-    <>
+    <div>
       <p>Test!</p>
-    </>
+      <div>
+        { /* Turns go here */}
+        { /* AddTurnForm */}
+      </div>
+      <div>
+        { /* Sidebar */ }
+      </div>
+    </div>
   )
 }
 
