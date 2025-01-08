956c0b13c6841894310738a914b49161f150a40f
oliver <oshmusic11@gmail.com>
2025-01-08 17:14:48 -0500

    Adjusted remove turn buttons

--- src/components/Turn.jsx
+++ src/components/Turn.jsx
@@ -1,10 +1,10 @@
 
 const Turn = ( { name, initiative, id, isCurrentTurn, removeTurn, refProp }) => {
     return (
-        <div id={`turn${id}`} className={isCurrentTurn ? "turn currentTurn" : "turn"} ref={refProp}>
-            <p>{name}</p>
+        <div id={`turn${id}`} className={isCurrentTurn ? "turn currentTurn" : "turn"}>
+            <p ref={refProp}>{name}</p>
             <p>{initiative}</p>
-            <button onClick={() => removeTurn(id)}>X</button>
+            <button onClick={() => removeTurn(id)} className="removeTurnButton"><span>x</span></button>
         </div>
     );
 }
