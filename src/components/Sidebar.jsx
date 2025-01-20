e91b856750687eab200abd26acb1ef4f63ad6427
oliver <oshmusic11@gmail.com>
2025-01-20 17:05:22 -0500

    Changed text for Previous and Next turn buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 const Sidebar = ({ currentTurnName, round, nextTurn, prevTurn }) => {
+    const prevButtonText = '<< Previous';
+    const nextButtonText = 'Next >>'
     return (
         <div>
             <p>Current turn: {currentTurnName}</p>
             <p>Round: {round}</p>
-            <button onClick={() => prevTurn()}>Previous Turn</button>
-            <button onClick={() => nextTurn()}>Next Turn</button>
+            <button onClick={() => prevTurn()}>{prevButtonText}</button>
+            <button onClick={() => nextTurn()}>{nextButtonText}</button>
         </div>
     );
 }
