0369b11fe1102212c7f6c0c1409830ff0a171b3a
oliver <oshmusic11@gmail.com>
2025-02-25 17:11:01 -0500

    Cleaned up code in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,13 @@ function App({ savedTheme }) {
   useEffect(() => {
     // Reset Index state if it goes out of bounds, updating Round state
     if (index === turns.length && turns.length > 0) {
-      //if we've reached the end of the list of turns, reset the index variable and increase the round counter
+      // If we've reached the end of the list of turns, reset the index variable and increase the round counter
       setIndex(0);
       setRound(round + 1);
     }
     // Scroll to make sure that current turn is within view
     if (turns.length > 0) {
-      //only scroll if turns state has at least one turn in it
+      // Only scroll if turns state has at least one turn in it
       if (index === 0) {
         scrollTo({ top: 0, left: 0, behavior: 'smooth' });
       }
@@ -59,39 +59,36 @@ function App({ savedTheme }) {
   });
 
   // Turns state
-  // const [turns, setTurns] = useState(testTurns); //starts prefilled for testing
   const [turns, setTurns] = useState([]);
 
   // Sorts the Turns state every time it's updated
   useEffect(() => {
     setTurns(prevTurns => prevTurns.sort(compareTurns));
-    // console.log(turns);
   }, [turns]);
 
   // Round state
   const [round, setRound] = useState(1);
 
   // Ref to generate ids for turns
-  const nextPlayerId = useRef(0); //FOR TESTING ONLY: STARTS AT 4. FOR AN EMPTY STARTING PLAYER STATE, CHANGE BACK TO 0
+  // Note to self: if using the prefilled test array above, set this to 4 instead of 0
+  const nextPlayerId = useRef(0); 
 
   // For tracking current turn
   const currentTurnRef = useRef();
 
-
+  // Theme state
   const [theme, setTheme] = useState(savedTheme);
 
+  // When Theme state changes, updates the body class and saves the current theme to persistent storage
   useEffect(() => {
     document.body.className = theme;
-    //writes to disk
+    // Writes to disk
     window.dataStoreAPI.saveTheme(theme);
   }, [theme]);
 
-
-
-
-  //helper function for sorting the Turns state
+  // Helper function for sorting the Turns state
   const compareTurns = (a, b) => {
-    //data cleaning:
+    // data cleaning:
     const aInit = parseInt(a.initiative);
     const bInit = parseInt(b.initiative);
     const aName = a.name.toLowerCase();
@@ -104,7 +101,7 @@ function App({ savedTheme }) {
       return 1; //b goes first
     }
     else {
-      //initiative counts are identical, sort alphabetically by name
+      // If initiative counts are identical, sorts alphabetically by name
       if (aName < bName) {
         return -1;
       }
@@ -112,18 +109,12 @@ function App({ savedTheme }) {
         return 1;
       }
       else {
-        return 0; //this is only reached if both turns are identical.
+        return 0; // This is only reached if both turns are identical.
       }
     }
   }
 
   const handleAddTurn = (name, initiative) => {
-    let prevIndId = null; //used only for tracking during testing at this point, but I'm leaving it in
-    //if the Turn is being added to a non-empty array, save the id of the current turn for later comparison
-    if (turns.length !== 0) {
-      prevIndId = turns[index].id;
-      // console.log(`Previous ind id: ${prevIndId}`);
-    }
     //prevents adding of blank turns
     if (name !== '' && initiative !== '') {
       setTurns(prevTurns => [
@@ -136,40 +127,23 @@ function App({ savedTheme }) {
       ]);
       setTurns(prevTurns => prevTurns.sort(compareTurns));
     }
-    //handles off-by-one errors when a new turn is added that appears before the current turn in the initiative order
-    //NOTE: Is this an error? That is desired behavior before the first round. So maybe this correcting only occurs if
-    // the user has clicked "Next Turn" at least once? (boolean state isRunning updated to True once clicked, never set to False)
-    // CURRENT PLAN: This isn't a bug, but I can try to fix it if the beta testers view it as such.
-    // if (prevIndId !== null) {
-    //   console.log(turns[index].id);
-    //   if (turns[index].id !== prevIndId) {
-    //     setIndex(index + 1);
-    //     console.log("index updated");
-    //   }
-    // }
-    //for testing
-    // console.log(`Current ind id: ${turns[index].id}`);
-    // console.log(turns);
   }
 
   const handleRemoveTurn = (id) => {
-    // console.log(`Removed ${id}`); //included for testing
     setTurns(prevturns => prevturns.filter(p => p.id !== id));
   }
 
   const handleNextTurn = () => {
-    // console.log(`previous index: ${turns[index].id}`)
     setIndex(index + 1);
-    // console.log(`current index = ${index}; turns length: ${turns.length}`);
   }
 
   const handlePreviousTurn = () => {
     if (index > 0) {
-      //If index is greater than 0, decrements it
+      // If index is greater than 0, decrements it
       setIndex(index - 1);
     }
     else {
-      //If index is 0, wraps around, decrementing the round (to a minimum of 1)
+      // If index is 0, wraps around, decrementing the round (to a minimum of 1)
       setIndex(turns.length - 1);
       if (round >= 2) {
         setRound(round - 1);
@@ -177,7 +151,7 @@ function App({ savedTheme }) {
     }
   }
 
-  //helper function to determine whether a Turn component is the current turn
+  // Helper function to determine whether a Turn component is the current turn
   const isCurrentTurnComponent = (id) => {
     if (index === turns.length) {
       return id === turns[0].id;
